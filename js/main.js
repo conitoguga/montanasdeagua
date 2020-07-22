aaae658279af80f9db32ce0834b82339e53dfd8e
Constanza Fuenzalida <Constanza@MacBook-Pro-de-Constanza.local>
2020-07-22 15:41:50 -0400

    prueba 9

--- js/main.js
+++ js/main.js
@@ -12,3 +12,32 @@ $('.menu nav a').on('click', function() {
     $('.menu').toggleClass('open')
 
 })
+
+
+let xs = []
+for (var i = 0; i <= 500; i++) {
+    xs.push(i)
+}
+
+let t = 0
+
+function animate() {
+    let points = xs.map(x => {
+        let y = 200 + 10 * Math.sin((x + t) / 15)
+
+
+        return [x, y]
+    })
+
+    let path = "M" + points.map(p => {
+        return p[0] + "," + p[1]
+    }).join("L")
+
+    document.querySelector(".mountain-path").setAttribute("d", path)
+
+    t += -1
+
+    requestAnimationFrame(animate)
+}
+
+animate()
