ff2f1f2fe91ff3dbc7ee40fb86e91042e0140a7f
Constanza Fuenzalida <Constanza@MacBook-Pro-de-Constanza.local>
2020-07-22 22:53:17 -0400

    prueba 10

--- js/main.js
+++ js/main.js
@@ -14,6 +14,21 @@ $('.menu nav a').on('click', function() {
 })
 
 
+window.onscroll = function() { myFunction() };
+
+var header = document.querySelector("header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+
+
 let xs = []
 for (var i = 0; i <= 500; i++) {
     xs.push(i)
